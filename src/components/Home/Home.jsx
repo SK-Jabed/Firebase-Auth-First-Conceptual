793cf6386e3fdf8572447087c087ef8c7cccef99
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-12 21:08:18 +0600

    Login with GitHub Authentication Done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,60 @@
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, TwitterAuthProvider } from 'firebase/auth';
 import React from 'react';
+import auth from '../../firebase.config';
 
 const Home = () => {
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+    const twitterProvider = new TwitterAuthProvider();
+
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, googleProvider)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.log("ERROR:", error);
+        })
+    }
+
+    const handleGithubLogin = () => {
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.log("ERROR:", error);
+        })
+    }
+
+    const handleTwitterLogin = () => {
+        signInWithPopup(auth, twitterProvider)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.log("ERROR:", error);
+        })
+    }
+
     return (
-        <div>
-            <h2>This is HomePage</h2>
-        </div>
+      <div className="flex justify-center items-center gap-4 mt-6">
+        <button
+          onClick={handleGoogleLogin}
+          className="btn btn-outline btn-accent"
+        >
+          Login with Google
+        </button>
+        <button onClick={handleGithubLogin} className="btn btn-warning">
+          Login with GitHub
+        </button>
+        <button
+          onClick={handleTwitterLogin}
+          className="btn btn-outline btn-primary"
+        >
+          Login with Twitter
+        </button>
+      </div>
     );
 };
 
--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+export const authContext = createContext();
 
 const MainLayout = () => {
     return (
         <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+            <authContext.Provider value={{name: "SKJ"}}>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </authContext.Provider>
         </div>
     );
 };
